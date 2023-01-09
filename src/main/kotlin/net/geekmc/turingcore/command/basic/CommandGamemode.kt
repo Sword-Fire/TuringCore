0f01290a566ed9ceee35836f2a2bfeaf577fc0aa
ShellWen <gofly233@gmail.com>
2023-01-10 03:32:02 +0800

    feat: 优化 CommandGamemode

--- src/main/kotlin/net/geekmc/turingcore/command/basic/CommandGamemode.kt
+++ src/main/kotlin/net/geekmc/turingcore/command/basic/CommandGamemode.kt
@@ -1,7 +1,6 @@
 package net.geekmc.turingcore.command.basic
 
 import net.geekmc.turingcore.command.args
-import net.geekmc.turingcore.command.basic.CommandGamemode.setGameMode
 import net.geekmc.turingcore.command.findPlayers
 import net.geekmc.turingcore.command.opSyntax
 import net.geekmc.turingcore.util.lang.sendLang
@@ -17,13 +16,24 @@ object CommandGamemode : Kommand({
 
     val targetArg = ArgumentEntity("target").onlyPlayers(true)
 
-    val modeArgs = ArgumentWord("mode").from(*kotlin.run {
-        mutableListOf<String>().apply {
-            addAll(GameMode.values().map { it.toString() })
-            addAll(GameMode.values().map { it.toString().lowercase() })
-            addAll(listOf("0", "1", "2", "3"))
-        }.toTypedArray()
-    })
+    val modeArgs = ArgumentWord("mode").from(
+        *listOf(
+            GameMode.values().map { it.toString() },
+            GameMode.values().map { it.toString().lowercase() },
+            listOf("0", "1", "2", "3")
+        ).flatten().toTypedArray()
+    )
+
+    fun setGameMode(sender: CommandSender, player: Player, mode: String) {
+        player.gameMode = when (mode.uppercase()) {
+            "0", "SURVIVAL" -> GameMode.SURVIVAL
+            "1", "CREATIVE" -> GameMode.CREATIVE
+            "2", "ADVENTURE" -> GameMode.ADVENTURE
+            "3", "SPECTATOR" -> GameMode.SPECTATOR
+            else -> error("unreachable.")
+        }
+        sender.sendLang("message-command-gamemode-succ", player.username, player.gameMode.name)
+    }
 
     opSyntax {
         sender.sendLang("message-command-wrong-usage")
@@ -47,16 +57,4 @@ object CommandGamemode : Kommand({
             setGameMode(sender, it, !modeArgs)
         }
     }
-}, name = "gamemode", aliases = arrayOf("gm")) {
-
-    fun setGameMode(sender: CommandSender, player: Player, mode: String) {
-        player.gameMode = when (mode.uppercase()) {
-            "0", "SURVIVAL" -> GameMode.SURVIVAL
-            "1", "CREATIVE" -> GameMode.CREATIVE
-            "2", "ADVENTURE" -> GameMode.ADVENTURE
-            "3", "SPECTATOR" -> GameMode.SPECTATOR
-            else -> error("unreachable.")
-        }
-        sender.sendLang("message-command-gamemode-succ", player.username, player.gameMode.name)
-    }
-}
+}, name = "gamemode", aliases = arrayOf("gm"))
