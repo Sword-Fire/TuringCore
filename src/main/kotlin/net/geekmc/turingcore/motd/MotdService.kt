7f1c8937cd823423a2246584a8a290c0461f9bb1
ShellWen <gofly233@gmail.com>
2023-01-26 02:20:23 +0800

    refactor: 优化 MotdService 代码

--- src/main/kotlin/net/geekmc/turingcore/motd/MotdService.kt
+++ src/main/kotlin/net/geekmc/turingcore/motd/MotdService.kt
@@ -11,11 +11,10 @@ import net.minestom.server.extensions.Extension
 import net.minestom.server.ping.ResponseData
 import org.kodein.di.instance
 import world.cepi.kstom.event.listenOnly
-import java.io.IOException
-import java.nio.file.Files
 import java.nio.file.Path
 import java.util.*
-import kotlin.io.path.exists
+import kotlin.io.path.notExists
+import kotlin.io.path.readBytes
 
 object MotdService : Service() {
 
@@ -30,10 +29,11 @@ object MotdService : Service() {
         extension.saveResource(ICON_PATH)
         extension.saveResource(MOTD_PATH)
         val motdConfig = YamlData(dataPath.resolve(MOTD_PATH), MotdService.javaClass.classLoader)
-        val descriptions = motdConfig.getOrElse<List<String>>("description") { emptyList() }
+        val descriptions: List<String> = motdConfig.getOrElse("description") { emptyList() }
         motdData = ResponseData().apply {
-            description = (descriptions[0] + "\n" + descriptions[1]).toComponent()
-            favicon = getIconAsBase64() ?: return@apply
+            // TODO: 是否应该硬编码两行
+            description = "${descriptions[0]}\n${descriptions[1]}".toComponent()
+            favicon = getIconAsBase64().getOrElse { "" }
         }
         EventNodes.DEFAULT.listenOnly<ServerListPingEvent> {
             responseData = motdData
@@ -45,15 +45,13 @@ object MotdService : Service() {
     /**
      * 获取经由 Base64 编码的图标。
      */
-    private fun getIconAsBase64(): String? {
+    private fun getIconAsBase64(): Result<String> = runCatching {
         val path = dataPath.resolve(ICON_PATH)
-        if (!path.exists()) return null
-        try {
-            val bytes = Files.readAllBytes(path)
-            return "data:image/png;base64,${Base64.getEncoder().encodeToString(bytes)}"
-        } catch (e: IOException) {
-            e.printStackTrace()
+        if (path.notExists()) error("Data path not exists.")
+
+        val base64 = path.readBytes().let { bytes ->
+            Base64.getEncoder().encodeToString(bytes)
         }
-        return null
+        return@runCatching "data:image/png;base64,$base64"
     }
 }
