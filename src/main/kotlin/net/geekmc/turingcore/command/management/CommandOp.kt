6e6eaebf219f68437d35bb7c37913ed602adbfbf
Leon <geekmc2022@gmail.com>
2023-01-25 11:53:04 +0800

    feat: 完成op系统；修复代码。

--- src/main/kotlin/net/geekmc/turingcore/command/management/CommandOp.kt
+++ src/main/kotlin/net/geekmc/turingcore/command/management/CommandOp.kt
@@ -2,7 +2,8 @@ package net.geekmc.turingcore.command.management
 
 import net.geekmc.turingcore.command.args
 import net.geekmc.turingcore.command.opSyntax
-import net.geekmc.turingcore.player.data
+import net.geekmc.turingcore.data.getData
+import net.geekmc.turingcore.player.essentialdata.EssentialPlayerData
 import net.geekmc.turingcore.util.color.message
 import net.minestom.server.command.builder.arguments.ArgumentBoolean
 import net.minestom.server.command.builder.arguments.minecraft.ArgumentEntity
@@ -26,7 +27,7 @@ object CommandOp : Kommand({
             sender.message("&r找不到玩家: ${args.getRaw(targetArg)}")
             return@opSyntax
         }
-        player.data["op"] = !booleanArg
+        player.getData<EssentialPlayerData>().isOp = !booleanArg
         sender.message("&g已将玩家玩家 &y${player.username} &g的管理员权限设置为 &r${!booleanArg}")
     }
 }, name = "operator", aliases = arrayOf("op"))
--- src/main/kotlin/net/geekmc/turingcore/player/essentialdata/EssentialPlayerDataService.kt
+++ src/main/kotlin/net/geekmc/turingcore/player/essentialdata/EssentialPlayerDataService.kt
@@ -1,40 +1,24 @@
-package net.geekmc.turingcore.player
+package net.geekmc.turingcore.player.essentialdata
 
-import kotlinx.serialization.Contextual
-import kotlinx.serialization.Serializable
-import net.geekmc.turingcore.data.PlayerData
 import net.geekmc.turingcore.data.PlayerDataService
+import net.geekmc.turingcore.data.dataOf
 import net.geekmc.turingcore.event.EventNodes
 import net.geekmc.turingcore.service.Service
-import net.minestom.server.coordinate.Pos
-import net.minestom.server.entity.GameMode
 import net.minestom.server.event.player.PlayerDisconnectEvent
 import net.minestom.server.event.player.PlayerLoginEvent
 import net.minestom.server.inventory.PlayerInventory
 import net.minestom.server.item.ItemStack
-import net.minestom.server.permission.Permission
 import world.cepi.kstom.event.listenOnly
 
 // TODO 存储Instance。玩家要先设置Instance，后允许teleport。
 object EssentialPlayerDataService : Service() {
 
-    @Serializable
-    data class EssentialPlayerData(
-        var isOp: Boolean = false,
-        var position: @Contextual Pos = Pos(0.0, 120.0, 0.0),
-        var gameMode: GameMode = GameMode.ADVENTURE,
-        var health: Float = 20f,
-        var permissions: Set<@Contextual Permission> = setOf(),
-        // 注意： player.inventory.size == INVENTORY_SIZE == 46 != INNER_INVENTORY_SIZE == 36
-        var inventory: ArrayList<ItemStack> = ArrayList<ItemStack>(PlayerInventory.INVENTORY_SIZE).apply {
-            fill(ItemStack.AIR)
-        }
-    ) : PlayerData()
+
 
     override fun onEnable() {
         PlayerDataService.register(EssentialPlayerData::class)
         EventNodes.VERY_HIGH.listenOnly<PlayerLoginEvent> {
-            val data = PlayerDataService.getData<EssentialPlayerData>(player)
+            val data = dataOf<EssentialPlayerData>(player)
             with(player) {
                 respawnPoint = data.position
                 gameMode = data.gameMode
