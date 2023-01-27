9d75627a9a1accc5be2ed9cc68b136401970c753
ShellWen <gofly233@gmail.com>
2023-01-28 00:50:33 +0800

    refactor: 优化 MotdService 健壮性与性能
    将 `motd/motd.yml` 重命名为 `motd/config.yml`，增加图片尺寸检查

--- src/main/kotlin/net/geekmc/turingcore/motd/MotdService.kt
+++ src/main/kotlin/net/geekmc/turingcore/motd/MotdService.kt
@@ -5,7 +5,9 @@ import net.geekmc.turingcore.di.PathKeys
 import net.geekmc.turingcore.event.EventNodes
 import net.geekmc.turingcore.service.Service
 import net.geekmc.turingcore.util.color.toComponent
+import net.geekmc.turingcore.util.extender.div
 import net.geekmc.turingcore.util.extender.saveResource
+import net.geekmc.turingcore.util.extender.warn
 import net.minestom.server.event.server.ServerListPingEvent
 import net.minestom.server.extensions.Extension
 import net.minestom.server.ping.ResponseData
@@ -13,27 +15,32 @@ import org.kodein.di.instance
 import world.cepi.kstom.event.listenOnly
 import java.nio.file.Path
 import java.util.*
+import kotlin.io.path.Path
+import kotlin.io.path.fileSize
 import kotlin.io.path.notExists
 import kotlin.io.path.readBytes
 
 object MotdService : Service() {
+    private val MOTD_PATH = Path("motd")
+    private val ICON_PATH = MOTD_PATH / "icon.png"
+    private val CONFIG_PATH = MOTD_PATH / "config.yml"
 
-    private const val ICON_PATH = "motd/icon.png"
-    private const val MOTD_PATH = "motd/motd.yml"
-
-    private lateinit var motdData: ResponseData
     private val extension by instance<Extension>()
     private val dataPath by instance<Path>(tag = PathKeys.EXTENSION_FOLDER)
 
     override fun onEnable() {
         extension.saveResource(ICON_PATH)
-        extension.saveResource(MOTD_PATH)
-        val motdConfig = YamlData(dataPath.resolve(MOTD_PATH), MotdService.javaClass.classLoader)
+        extension.saveResource(CONFIG_PATH)
+        val motdConfig = YamlData(dataPath / CONFIG_PATH, MotdService.javaClass.classLoader)
         val descriptions: List<String> = motdConfig.getOrElse("description") { emptyList() }
-        motdData = ResponseData().apply {
+        require(descriptions.size <= 2) { "Motd description must have at most two lines." }
+        val motdData = ResponseData().apply {
             // TODO: 是否应该硬编码两行
-            description = "${descriptions[0]}\n${descriptions[1]}".toComponent()
-            favicon = getIconAsBase64().getOrElse { "" }
+            description = descriptions.joinToString("\n").toComponent()
+            favicon = getIconAsBase64().getOrElse {
+                logger.warn("Failed to load icon. Use empty.", it)
+                ""
+            }
         }
         EventNodes.DEFAULT.listenOnly<ServerListPingEvent> {
             responseData = motdData
@@ -46,8 +53,11 @@ object MotdService : Service() {
      * 获取经由 Base64 编码的图标。
      */
     private fun getIconAsBase64(): Result<String> = runCatching {
-        val path = dataPath.resolve(ICON_PATH)
-        if (path.notExists()) error("Data path not exists.")
+        val path = dataPath / ICON_PATH
+        if (path.notExists()) error("Icon path not exists.")
+
+        if (path.fileSize() > 64 * 1024.0)
+            logger.warn { "Icon size is larger than 64KB. Watch out for performance issues." }
 
         val base64 = path.readBytes().let { bytes ->
             Base64.getEncoder().encodeToString(bytes)
--- src/main/kotlin/net/geekmc/turingcore/util/extender/Extension.kt
+++ src/main/kotlin/net/geekmc/turingcore/util/extender/Extension.kt
@@ -1,12 +1,18 @@
 package net.geekmc.turingcore.util.extender
 
 import net.minestom.server.extensions.Extension
+import java.nio.file.Path
 import kotlin.io.path.exists
 
-fun Extension.saveResource(resource: String, throwWhenExists: Boolean = false) {
-    val target = dataDirectory.resolve(resource)
-    if (target.exists()) {
-        throwWhenExists && error("Resource $resource already exists!")
+fun Extension.saveResource(resource: String, throwWhenExists: Boolean = false): Boolean =
+    saveResource(Path.of(resource), throwWhenExists)
+
+fun Extension.saveResource(resourcePath: Path, throwWhenExists: Boolean = false): Boolean {
+    val target = dataDirectory / resourcePath
+    return if (target.exists()) {
+        throwWhenExists && error("Resource $resourcePath already exists!")
+        false
+    } else {
+        savePackagedResource(resourcePath)
     }
-    savePackagedResource(resource)
 }
