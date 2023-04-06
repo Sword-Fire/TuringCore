41e1c15deb9825d37e67b96b7f21dc3d8edc1673
hanbings <hanbings@hanbings.io>
2023-04-07 02:33:05 +0800

    feat: 实现等级系统的关键 API

--- src/main/kotlin/net/geekmc/turingcore/level/LevelConfig.kt
+++ src/main/kotlin/net/geekmc/turingcore/level/LevelConfig.kt
@@ -12,11 +12,29 @@ data class LevelConfig(
 /**
  * 等级表
  * @param main 是否为主等级表
+ * @param display 是否在经验条显示等级
+ * @param type 等级计算类型
  * @param shadow 经验值与等级映射表
+ * @param formula 计算值与等级映射表公式
+ * @param maxLevel 等级上限
+ * @param maxExp 经验值上限
  */
 @Serializable
 data class LevelTable(
     val main: Boolean,
     val display: Boolean,
-    val shadow: HashMap<Int, Int> = HashMap()
+    val type: LevelType,
+    val shadow: HashMap<Int, Int> = HashMap(),
+    val formula: String,
+    val maxLevel: Int,
+    val maxExp: Int
 )
+
+@Serializable
+enum class LevelType {
+    // 映射
+    SHADOW,
+
+    // 公式
+    FORMULA
+}
--- src/main/kotlin/net/geekmc/turingcore/level/LevelService.kt
+++ src/main/kotlin/net/geekmc/turingcore/level/LevelService.kt
@@ -1,6 +1,7 @@
 package net.geekmc.turingcore.level
 
 import net.geekmc.turingcore.level.event.PlayerExpChangedEvent
+import net.geekmc.turingcore.level.event.PlayerLevelChangedEvent
 import net.geekmc.turingcore.library.config.ConfigService
 import net.geekmc.turingcore.library.data.player.PlayerDataLoadEvent
 import net.geekmc.turingcore.library.data.player.PlayerDataService
@@ -14,7 +15,14 @@ import net.minestom.server.extensions.Extension
 import org.kodein.di.instance
 import world.cepi.kstom.event.listenOnly
 
+/**
+ * 等级系统服务
+ *
+ * 当经验值出现变化后触发 [PlayerExpChangedEvent]
+ * 当等级出现变化后触发 [PlayerLevelChangedEvent]
+ */
 @AutoRegister
+@Suppress("unused")
 object LevelService : Service(turingCoreDi) {
     private val extension: Extension by instance()
     private lateinit var config: LevelConfig
@@ -51,6 +59,28 @@ object LevelService : Service(turingCoreDi) {
         require(isLevelExist(type)) { "Level type $type does not exist" }
         require(exp < 0) { "The target value $exp cannot be less than 0." }
 
+        val data: LevelData = PlayerDataService.getPlayerData(player.uuid, LevelData::class)
+        val level = data.levels[type]!!
+
+        val original = level.exp
+
+        // 检查是否超过最大经验值
+        require(level.exp + exp > config.levels[type]!!.maxExp) {
+            "The target value $exp cannot be greater than ${config.levels[type]!!.maxExp}."
+        }
+
+        level.exp = level.exp + exp
+
+        // 处理升降级
+        calculateLevel(type, data)
+
+        if (config.levels[type]!!.main && config.levels[type]!!.display) {
+            player.exp = level.exp.toFloat()
+            player.level = level.level
+        }
+
+        EventDispatcher.call(PlayerExpChangedEvent(player, type, original, level.exp))
+
         return 0
     }
 
@@ -64,6 +94,26 @@ object LevelService : Service(turingCoreDi) {
     fun removeExp(type: String, player: Player, exp: Int): Int {
         require(isLevelExist(type)) { "Level type $type does not exist" }
         require(exp < 0) { "The target value $exp cannot be less than 0." }
+
+        val data: LevelData = PlayerDataService.getPlayerData(player.uuid, LevelData::class)
+        val level = data.levels[type]!!
+
+        val original = level.exp
+
+        // 检查是否小于 0
+        require(level.exp - exp < 0) { "The target value $exp cannot be less than 0." }
+
+        level.exp = level.exp - exp
+
+        // 处理升降级
+        calculateLevel(type, data)
+
+        if (config.levels[type]!!.main && config.levels[type]!!.display) {
+            player.exp = level.exp.toFloat()
+            player.level = level.level
+        }
+
+        EventDispatcher.call(PlayerExpChangedEvent(player, type, original, level.exp))
 
         return 0
     }
@@ -79,6 +129,27 @@ object LevelService : Service(turingCoreDi) {
         require(isLevelExist(type)) { "Level type $type does not exist" }
         require(level < 0) { "The target value $level cannot be less than 0." }
 
+        val data: LevelData = PlayerDataService.getPlayerData(player.uuid, LevelData::class)
+        val levelData = data.levels[type]!!
+
+        val original = levelData.level
+
+        // 检查是否超过最大等级
+        require(levelData.level + level > config.levels[type]!!.maxLevel) {
+            "The target value $level cannot be greater than ${config.levels[type]!!.maxLevel}."
+        }
+
+        levelData.level = levelData.level + level
+
+        calculateExp(type, data)
+
+        if (config.levels[type]!!.main && config.levels[type]!!.display) {
+            player.exp = levelData.exp.toFloat()
+            player.level = levelData.level
+        }
+
+        EventDispatcher.call(PlayerLevelChangedEvent(player, type, original, levelData.level))
+
         return 0
     }
 
@@ -93,6 +164,25 @@ object LevelService : Service(turingCoreDi) {
         require(isLevelExist(type)) { "Level type $type does not exist" }
         require(level < 0) { "The target value $level cannot be less than 0." }
 
+        val data: LevelData = PlayerDataService.getPlayerData(player.uuid, LevelData::class)
+        val levelData = data.levels[type]!!
+
+        val original = levelData.level
+
+        // 检查是否小于 0
+        require(levelData.level - level < 0) { "The target value $level cannot be less than 0." }
+
+        levelData.level = levelData.level - level
+
+        calculateExp(type, data)
+
+        if (config.levels[type]!!.main && config.levels[type]!!.display) {
+            player.exp = levelData.exp.toFloat()
+            player.level = levelData.level
+        }
+
+        EventDispatcher.call(PlayerLevelChangedEvent(player, type, original, levelData.level))
+
         return 0
     }
 
@@ -105,6 +195,9 @@ object LevelService : Service(turingCoreDi) {
     fun setLevel(type: String, player: Player, level: Int): Int {
         require(isLevelExist(type)) { "Level type $type does not exist" }
         require(level < 0) { "The target value $level cannot be less than 0." }
+        require(level > config.levels[type]!!.maxLevel) {
+            "The target value $level cannot be greater than ${config.levels[type]!!.maxLevel}."
+        }
 
         val data: LevelData = PlayerDataService.getPlayerData(player.uuid, LevelData::class)
         val levelData = data.levels[type]!!
@@ -112,13 +205,15 @@ object LevelService : Service(turingCoreDi) {
         val original = levelData.level
         levelData.level = level
 
-        // 处理升降级
+        // 处理经验值
+        calculateExp(type, data)
 
         if (config.levels[type]!!.main && config.levels[type]!!.display) {
+            player.exp = levelData.exp.toFloat()
             player.level = levelData.level
         }
 
-        EventDispatcher.call(PlayerExpChangedEvent(player, type, original, levelData.level))
+        EventDispatcher.call(PlayerLevelChangedEvent(player, type, original, levelData.level))
 
         return 0
     }
@@ -133,6 +228,9 @@ object LevelService : Service(turingCoreDi) {
     fun setExp(type: String, player: Player, exp: Int): Int {
         require(isLevelExist(type)) { "Level type $type does not exist" }
         require(exp < 0) { "The target value $exp cannot be less than 0." }
+        require(exp > config.levels[type]!!.maxExp) {
+            "The target value $exp cannot be greater than ${config.levels[type]!!.maxExp}."
+        }
 
         val data: LevelData = PlayerDataService.getPlayerData(player.uuid, LevelData::class)
         val level = data.levels[type]!!
@@ -141,14 +239,68 @@ object LevelService : Service(turingCoreDi) {
         level.exp = exp
 
         // 处理升降级
+        calculateLevel(type, data)
 
         if (config.levels[type]!!.main && config.levels[type]!!.display) {
             player.exp = level.exp.toFloat()
+            player.level = level.level
         }
 
         EventDispatcher.call(PlayerExpChangedEvent(player, type, original, level.exp))
 
         return 0
+    }
+
+    /**
+     * 计算正确的经验值
+     * @param data 玩家等级数据
+     */
+    private fun calculateExp(type: String, data: LevelData) {
+        val levelData = data.levels[type]!!
+
+        if (config.levels[type]!!.type == LevelType.SHADOW) {
+            // 计算 level 处于等级映射表中的哪一个部分 并给予经验值
+            val levelMap = config.levels[type]!!.shadow
+            val levelMapKeys = levelMap.keys
+            val levelMapKeysList = levelMapKeys.toList()
+            val levelMapKeysListSize = levelMapKeysList.size
+            for (i in 0 until levelMapKeysListSize) {
+                if (levelMapKeysList[i] > levelData.level) {
+                    if (i == 0) {
+                        levelData.exp = 0
+                    } else {
+                        levelData.exp = levelMap[levelMapKeysList[i - 1]]!!
+                    }
+                    break
+                }
+            }
+        } else {
+            // TODO: 使用公式计算经验值
+        }
+    }
+
+    private fun calculateLevel(type: String, data: LevelData) {
+        val levelData = data.levels[type]!!
+
+        if (config.levels[type]!!.type == LevelType.SHADOW) {
+            // 计算经验值在那一部分给予正确的等级
+            val levelMap = config.levels[type]!!.shadow
+            val levelMapValues = levelMap.values
+            val levelMapValuesList = levelMapValues.toList()
+            val levelMapValuesListSize = levelMapValuesList.size
+            for (i in 0 until levelMapValuesListSize) {
+                if (levelMapValuesList[i] > levelData.exp) {
+                    if (i == 0) {
+                        levelData.level = 0
+                    } else {
+                        levelData.level = levelMap.keys.toList()[i - 1]
+                    }
+                    break
+                }
+            }
+        } else {
+            // TODO: 使用公式计算等级
+        }
     }
 
     /**
