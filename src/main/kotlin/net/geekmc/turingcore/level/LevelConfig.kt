8695e5f249374c105b5b83d07433fb5189df1c28
hanbings <hanbings@hanbings.io>
2023-04-06 10:57:50 +0800

    refactor: 为等级系统添加多等级支持

--- src/main/kotlin/net/geekmc/turingcore/level/LevelConfig.kt
+++ src/main/kotlin/net/geekmc/turingcore/level/LevelConfig.kt
@@ -5,5 +5,18 @@ import net.geekmc.turingcore.library.config.Config
 
 @Serializable
 data class LevelConfig(
-    val levels: HashMap<Long, Long> = HashMap()
+    val levels: Set<LevelTable> = HashSet()
 ) : Config()
+
+/**
+ * 等级表
+ * @param name 等级表名称
+ * @param main 是否为主等级表
+ * @param shadow 经验值与等级映射表
+ */
+@Serializable
+data class LevelTable(
+    val name: String,
+    val main: Boolean,
+    val shadow: Map<Long, Long> = HashMap()
+)
--- src/main/kotlin/net/geekmc/turingcore/level/LevelService.kt
+++ src/main/kotlin/net/geekmc/turingcore/level/LevelService.kt
@@ -1,6 +1,5 @@
 package net.geekmc.turingcore.level
 
-import net.geekmc.turingcore.coin.CoinData
 import net.geekmc.turingcore.library.config.ConfigService
 import net.geekmc.turingcore.library.data.player.PlayerDataLoadEvent
 import net.geekmc.turingcore.library.di.turingCoreDi
@@ -15,13 +14,19 @@ import world.cepi.kstom.event.listenOnly
 @AutoRegister
 object LevelService : Service(turingCoreDi) {
     private val extension: Extension by instance()
+
+    // 缓存玩家数据
+    private val players = HashMap<Player, LevelData>()
     private lateinit var config: LevelConfig
 
     override fun onEnable() {
         loadConfig()
         EventNodes.VERY_HIGH.listenOnly<PlayerDataLoadEvent> {
-            val data = getData<CoinData>()
-
+            val data = getData<LevelData>()
+            // 缓存玩家数据
+            Player.getEntity(uuid)?.let {
+                players[it as Player] = data
+            }
         }
     }
 
@@ -31,41 +36,83 @@ object LevelService : Service(turingCoreDi) {
 
     /**
      * 为一个玩家添加经验值
+     * @param name 等级表名称
      * @param player 玩家
      * @param exp 经验值
      * @return 增加后的经验值
      */
-    fun addExp(player: Player, exp: Long): Long {
+    fun addExp(name: String, player: Player, exp: Long): Long {
         return 0
     }
 
     /**
      * 为玩家移除一些经验值
+     * @param name 等级表名称
      * @param player 玩家
      * @param exp 经验值
      * @return 移除后的经验值
      */
-    fun removeExp(player: Player, exp: Long): Long {
+    fun removeExp(name: String, player: Player, exp: Long): Long {
         return 0
     }
 
     /**
      * 为玩家添加等级
+     * @param name 等级表名称
      * @param player 玩家
      * @param level 等级
      * @return 移除玩家的等级
      */
-    fun addLevel(player: Player, level: Long): Long {
+    fun addLevel(name: String, player: Player, level: Long): Long {
         return 0
     }
 
     /**
      * 为玩家移除一些等级
+     * @param name 等级表名称
      * @param player 玩家
      * @param level 等级
      * @return 移除后的玩家等级
      */
-    fun removeLevel(player: Player, level: Long): Long {
+    fun removeLevel(name: String, player: Player, level: Long): Long {
         return 0
+    }
+
+    /**
+     * 设置玩家的等级
+     * @param name 等级表名称
+     * @param player 玩家
+     * @return 玩家的等级
+     */
+    fun setLevel(name: String, player: Player, level: Long): Long {
+        return 0
+    }
+
+    /**
+     * 设置玩家的经验值
+     * @param name 等级表名称
+     * @param player 玩家
+     * @param exp 经验值
+     * @return 设置后的经验值
+     */
+    fun setExp(name: String, player: Player, exp: Long): Long {
+        return 0
+    }
+
+    /**
+     * 允许插件注册一个等级系统
+     * @param name 等级系统名称
+     * @param levels 等级与经验值的映射
+     */
+    fun register(name: String, levels: HashMap<Long, Long>) {
+
+    }
+
+    /**
+     * 允许插件注销一个等级系统
+     * @param type 等级系统名称
+     */
+    fun unregister(type: String) {
+
     }
 }
